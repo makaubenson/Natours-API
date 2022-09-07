1e125381b9e1c6dcbec5f3702d9ac9f96b9245b1
Benson <bensonmakau2000@gmail.com>
2022-09-07 16:25:30 +0300

    handling patch requests

--- app.js
+++ app.js
@@ -74,6 +74,23 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+//Patch
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Uploaded Here>',
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
