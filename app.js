0f44a1b8f2d83b3274a00ea5b4125924a1cac475
Benson <bensonmakau2000@gmail.com>
2022-09-07 16:30:29 +0300

    handling delete requests

--- app.js
+++ app.js
@@ -75,7 +75,24 @@ app.post('/api/v1/tours', (req, res) => {
 });
 
 //Patch
-app.patch('/api/v1/tours/:id', (req, res) => {
+// app.patch('/api/v1/tours/:id', (req, res) => {
+//   if (req.params.id * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID',
+//     });
+//   }
+
+//   res.status(200).json({
+//     status: 'success',
+//     data: {
+//       tour: '<Uploaded Here>',
+//     },
+//   });
+// });
+
+//DElete
+app.delete('/api/v1/tours/:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -83,11 +100,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     });
   }
 
-  res.status(200).json({
+  res.status(204).json({
     status: 'success',
-    data: {
-      tour: '<Uploaded Here>',
-    },
+    data: null,
   });
 });
 
