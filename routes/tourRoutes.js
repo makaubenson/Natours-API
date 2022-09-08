b93c501f7f22175190ac5fc3556730c046a93e91
Benson <bensonmakau2000@gmail.com>
2022-09-08 11:25:00 +0300

    creating controllers and linking them with routes

---
+++ routes/tourRoutes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+
+//Method 2
+// const {
+//   getTour,
+//   createTour,
+//   updateTour,
+//   deleteTour,
+//   getAllTours,
+// } = require('./../controllers/tourController');
+
+const router = express.Router();
+
+//Method 1
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .post(tourController.createTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+//Method 2
+// router.route('/').get(getAllTours).post(createTour);
+// router
+//   .route('/:id')
+//   .get(getTour)
+//   .post(createTour)
+//   .patch(updateTour)
+//   .delete(deleteTour);
+
+module.exports = router;
