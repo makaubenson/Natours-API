cbd3ac882e41742a71c71926e08c597fe5e0eb9a
Benson <bensonmakau2000@gmail.com>
2022-09-09 16:15:13 +0300

    connection express app with mongoDB

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 //Imports
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
 // console.log(app.get('env'));
-
 // console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// .connect(process.env.DATABASE_LOCAL, {
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    // console.log(con);
+    console.log('DB Connection Successful');
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
