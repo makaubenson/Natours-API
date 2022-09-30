20c7f6191a1d83f6a4abacfb9031ff003a4e5b1f
benson <bensonmakau2000@gmail.com>
2022-09-30 18:18:32 +0300

    created user schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
+// const validator = require('validator');
 
 //Basic Tour Schema
 const tourSchema = new mongoose.Schema(
---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+//create user schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true, //ensure no same email will exist for different users
+    lowercase: true, //transform email to lowercase
+    validate: [validator.isEmail, 'Please provide a valid email'], //validate email
+  },
+  photo: {
+    //will store path to photo in the file system
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: 8, //have min of 8 chars for a password
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
