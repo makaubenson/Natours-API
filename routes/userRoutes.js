de8089cfb15753e765872343cb871644c50a53c0
benson <bensonmakau2000@gmail.com>
2022-10-12 16:22:12 +0300

    revising user model
    password hashing, creation users, salting, modelling users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-//doesnt follow REST architecture
+//doesn't follow REST architecture
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
