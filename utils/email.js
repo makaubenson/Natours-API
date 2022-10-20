ade688cadf693d45dcc6052c371af2d6d751a8fe
benson <bensonmakau2000@gmail.com>
2022-10-20 10:34:10 +0300

    generating password reset token

--- utils/email.js
+++ utils/email.js
@@ -1,15 +1,26 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = (options) => {
+const sendEmail = async (options) => {
   //1) Create a transporter
   const transporter = nodemailer.createTransport({
-    service: 'Gmail', //yahoo, hotmail etc
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
-    //activate in gmail "less secure app" option
   });
   //2)define the email options
+
+  const mailOptions = {
+    from: 'Benson Makau <hello@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.text,
+    //html
+  };
   //3)actually send the email
+  await transporter.sendMail(mailOptions);
 };
+
+module.exports = sendEmail;
