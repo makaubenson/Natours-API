16c856db080c24ed4c97e4304e61f4cbd1fb6c9c
benson <bensonmakau2000@gmail.com>
2022-10-23 10:48:13 +0300

    parent referencing

---
+++ models/reviewModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour', // reference
+      required: [true, 'Review must belong to a tour.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+  },
+  //virtual properties: makes sure that if we have a field that isnt stored in DB
+  //   but calculated using some other value, we want it to show on the output
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
