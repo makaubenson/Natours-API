de1305a53b9d47ca0898ee8439e879ab81d9be04
benson <bensonmakau2000@gmail.com>
2022-10-23 11:20:47 +0300

    Creating and Getting reviews

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -77,6 +78,7 @@ app.use((req, res, next) => {
 //3) Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 //handling non-existent routes (should be the last after all other routes handlers)
 app.all('*', (req, res, next) => {
