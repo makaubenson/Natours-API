fc9247a1495f4628b8ca47903f0a3d3dfeda9e29
benson <bensonmakau2000@gmail.com>
2022-10-24 17:55:30 +0300

    importing review and user data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,51 +1,52 @@
-//Imports
+const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const fs = require('fs');
 const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
 
-dotenv.config({ path: './config.env' }); //Importing environment variables
-// console.log(app.get('env'));
-// console.log(process.env);
+dotenv.config({ path: './config.env' });
 
-//Database Connection String
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-//Database Connection
-// .connect(process.env.DATABASE_LOCAL, {
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => {
-    // console.log(con.connections);
-    console.log('DB Connection Successful');
-  });
+  .then(() => console.log('DB connection successful!'));
 
-//Read JSON file
+// READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
-//Import Data into Database
+// IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
-    console.log('Data Successfully Loaded');
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
+    console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
   }
   process.exit();
 };
 
-//Delete all Data from DB
+// DELETE ALL DATA FROM DB
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
-    console.log('Data Deleted successfully');
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
   }
