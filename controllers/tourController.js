34afaa4a6f991a1190e30163a92154f41fbc20fe
benson <bensonmakau2000@gmail.com>
2022-10-24 16:07:52 +0300

    factory functions: create

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -52,29 +52,8 @@ exports.getTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createTour = catchAsync(async (req, res, next) => {
-  const newTour = await Tour.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
-});
-
+exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
-
-// exports.deleteTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndDelete(req.params.id);
-//   if (!tour) {
-//     return next(new AppError('No tour found with that ID', 404));
-//   }
-//   res.status(204).json({
-//     status: 'success',
-//     data: null,
-//   });
-// });
 exports.deleteTour = factory.deleteOne(Tour);
 
 //aggregation pipeline use case
