cf4235830f7b3d23db04fde6c439996a354e50c1
benson <bensonmakau2000@gmail.com>
2022-10-24 17:21:02 +0300

    adding missing authorization and authentication

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,6 +4,9 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+//all routes below this line are protected
+router.use(authController.protect);
+
 //POST /tour/23gt4g/reviews
 //GET /tour/23gt4g/reviews
 //POST /reviews
@@ -12,7 +15,6 @@ router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -21,7 +23,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,24 +7,20 @@ const router = express.Router();
 //doesn't follow REST architecture
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+//all routes below this line will be protected since middleware comes in sequence
+router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+//all routes below this line are restricted to admins
+router.use(authController.restrictTo('admin'));
 
 //Follows REST Architecture
 router
