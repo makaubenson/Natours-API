5719fc91d1505a45afdd57756e2d114b998a9e97
benson <bensonmakau2000@gmail.com>
2022-11-09 11:29:35 +0300

    building the tour overview
    part 1

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -76,14 +77,7 @@ app.use((req, res, next) => {
 });
 
 //3) Routes
-app.get('/',(req,res) => {
-  res.status(200).render('base', {
-    // these values below are called locals in pug
-    tour: 'The Forest Hiker',
-    user: 'Benson'
-  })
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
