69c4d6e786e2dc2f0db366374fbb63ef48aa80bc
benson <bensonmakau2000@gmail.com>
2022-11-25 12:23:13 +0300

    resizing images using Sharp Library

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,24 @@
 const multer = require('multer');
+const sharp = require('sharp');//image processing library
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
 //multerStorage: define filename of the uploaded file and where to store it.
-const multerStorage = multer.diskStorage({
-  destination:(req,file, cb) => {
-    cb(null,'public/img/users' );
-  },
-  filename: function (req, file, cb) {
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  }
-})
+// const multerStorage = multer.diskStorage({
+//   destination:(req,file, cb) => {
+//     cb(null,'public/img/users' );
+//   },
+//   filename: function (req, file, cb) {
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   }
+// })
+
+
+//store image on memory as buffer
+const multerStorage = multer.memoryStorage();
 
 //multerFilter: to check if uploaded file is an image
 const multerFilter = (req,file,cb) => {
@@ -31,6 +36,20 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req,res,next) => {
+  if(!req.file) return next();
+
+req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+sharp(req.file.buffer)
+.resize(500,500)
+.toFormat('jpeg')
+.jpeg({quality:90})
+.toFile(`public/img/users/${req.file.filename}`);
+
+next();
+}
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
