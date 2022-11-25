bcda48d82468ba1c8fea388edd7e02f2a3174fb0
benson <bensonmakau2000@gmail.com>
2022-11-25 17:27:13 +0300

    building a complex email handler

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,58 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+// new Email(user, url).sendWelcome();
+
+module.exports = class Email {
+  constructor(user,url){
+this.to = user.email;
+this.firstName = user.name.split(' ')[0];
+this.url = url;
+this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`
+  }
+
+  newTransport(){
+    if(process.env.NODE_ENV === 'production'){
+      //sendgrid
+      return 1;
     }
-  });
+  return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-  // 2) Define the email options
+   //send actual email
+ async send(template,subject){
+ //1) render HTML based on a pug template
+const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+  firstName: this.firstName,
+  url: this.url,
+  subject
+})
+
+//2)define email options
   const mailOptions = {
-    from: 'Jonas Schmedtmann <hello@jonas.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
+    from: this.from,
+    to: this.to,
+    subject:subject,
+    html: html,
+    text: htmlToText.fromString(html)
   };
 
-  // 3) Actually send the email
-  await transporter.sendMail(mailOptions);
+  //3) Create a transport and send email
+await this.newTransport.sendMail(mailOptions);
+}
+
+ async sendWelcome(){
+ await this.send(`welcome`,`Welcome to tha Natours Family!`)
+  }
 };
 
-module.exports = sendEmail;
+
+
