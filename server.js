9972fd41f1c0a5f7267db2798f706842d9a25b1e
benson <bensonmakau2000@gmail.com>
2022-11-27 00:59:15 +0300

    changed mongo URI

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+const DB =`mongodb+srv://makaubenson:pHG1eOmdNyhobQF6@cluster0.zxwe44r.mongodb.net/natours?retryWrites=true&w=majority`;
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
