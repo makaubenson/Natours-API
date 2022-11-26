dcc844af0bd06d0844e01307ed5a8192df04ddf0
benson <bensonmakau2000@gmail.com>
2022-11-26 23:31:09 +0300

    removed unnecessary code

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', err => {
-  // console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  // console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
   process.exit(1);
 });
 
@@ -26,12 +26,12 @@ mongoose
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  // console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', err => {
-  // console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  // console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
