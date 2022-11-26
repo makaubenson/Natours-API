6ccf68337f3e12844bc1c06057ec65bd1a8ddac8
benson <bensonmakau2000@gmail.com>
2022-11-27 01:30:11 +0300

    changed node engine

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
