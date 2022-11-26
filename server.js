042ca2ad4b4ba33b659143830bd94ecfcb95b934
benson <bensonmakau2000@gmail.com>
2022-11-27 01:36:49 +0300

    changed import of dotenv

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
