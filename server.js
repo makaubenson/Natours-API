7fa6be784d3bfbef4ddf091c110e1adcf32b12f2
benson <bensonmakau2000@gmail.com>
2022-11-27 00:41:23 +0300

    server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
+  console.log(DB, `THIS IS IT`);
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
